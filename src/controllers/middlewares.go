b45d0a840dd7586701f72abb368744e7e9ac86b6
manav1403 <manavavni@gmail.com>
2021-02-19 16:11:26 +0530

    added midllewares and JWT auth functions

---
+++ src/controllers/middlewares.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"errors"
+	"net/http"
+
+	"myURL.com/inventory/helpers"
+)
+
+//SetMiddlewareJSON sets the header content type
+func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next(w, r)
+	}
+}
+
+//SetMiddlewareAuthentication checks for token in request
+func SetMiddlewareAuthentication(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := helpers.TokenValid(r)
+		if err != nil {
+			helpers.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+			return
+		}
+		next(w, r)
+	}
+}
