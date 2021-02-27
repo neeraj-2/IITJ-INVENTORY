c8c3b1c8208cbf077c5662ea25706face4e789f2
manav1403 <manavavni@gmail.com>
2021-02-27 20:30:12 +0530

    added uuid to user

--- src/models/User.go
+++ src/models/User.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
+	uuid "github.com/satori/go.uuid"
 )
 
 // User Model
@@ -10,5 +11,12 @@ type User struct {
 	Name    string `gorm:"not null"`
 	Email   string `gorm:"not null"`
 	Issued  []Issued
-	isAdmin bool `gorm: "DEFAULT:false"`
+	isAdmin bool      `gorm: "DEFAULT:false"`
+	UUID    uuid.UUID `gorm:"primaryKey"`
+}
+
+//BeforeCreate for user
+func (user *User) BeforeCreate(scope *gorm.Scope) error {
+	uuid := uuid.NewV4()
+	return scope.SetColumn("UUID", uuid)
 }
